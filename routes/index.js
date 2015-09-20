6587393bee8d437985c6ee6cc9359b6cf310a2ea
ThibBal <thibault.balmette@isep.fr>
2015-09-20 14:08:01 -0400

    Fin du TP1

--- routes/index.js
+++ routes/index.js
@@ -6,37 +6,41 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Les Grottes de Kalte' });
 });
 
+/* GET help page. */
 router.get('/help', function(req, res) {
     res.render('help', { title: "Aide" });
 });
 
-router.get('/begin', function(req, res) {
+/* GET createPlayer page. */
+router.get('/createPlayer', function(req, res) {
     res.render('createPlayer', { title: "Création du joueur" });
 });
 
-router.post('/createPlayer', function(req, res) {
+/* POST begin page. */
+router.post('/begin', function(req, res) {
   var disciplines = req.body.disciplines;
   var equipements = req.body.equipements;
 
-  // TO DO
+  // TO DO : create a session with the values "disciplines" and "equipements"
   console.log("Les disciplines sont : " + disciplines);
+  console.log("Les équipements sont : " + equipements);
+
+  // Redirect the player to the first page of the book
 	res.redirect('./page/1');
 });
 
-/* GET page page. */
-// http://stackoverflow.com/questions/20089582/how-to-get-url-parameter-in-express-node-js
-// http://stackoverflow.com/questions/12132978/use-a-variable-in-a-jade-include
+/* GET page (with the right number) page. */
+// see @exempleExpress on Moodle
 router.get('/page/:value', function(req, res, next) {
-  // On récupère le paramètre de l'URL
-  var v = req.params.value
+  // We get the parameter "value" from our request
+  var numberPage = req.params.value
 
-  // On crée dynamiquement la page qu'on souhaite charger
-  var page = "./page" + v + ".jade"
+  // We create the "right" page with the value
+  var page = "./page" + numberPage + ".jade"
 
-  // On veut d'abord convertir la page en HTML, une fois que la conversion
-  // est faite, on va injecter le HTML généré vers le fichier page.jade
+  // The page is converted to HTML and then put in the page.jade
   res.render(page, function(err, html) {
-      res.render('page', { title: v, htmlPage: html })
+      res.render('page', { title: numberPage, htmlPage: html })
   });
 });
 
