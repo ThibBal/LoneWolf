05d4103664702997ad05c5ee5dd2ba262a77565f
ThibBal <tbalmette@gmail.com>
2015-11-13 13:40:57 -0500

    [TP5] Ajout d'angular (fichier)

---
+++ controllers/CreationJoueurController.js
@@ -0,0 +1,7 @@
+var app = angular.module('LoneWolf', []);
+
+app.controller('validationFormulaire', function($scope) {
+	$scope.title = "Création de joueur";
+/*	$scope.message = function(c) { return "Bienvenue au cours " + c; };
+*/
+});
